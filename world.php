59644db842fb43c26a85e152728b3d03461c080a
caysi morgan <caysi.s.morgan@gmail.com>
2022-11-14 23:21:58 -0500

    final submission

--- world.php
+++ world.php
@@ -1,17 +1,60 @@
 <?php
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
 
-$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $lab5_user, $password123);
+$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 $stmt = $conn->query("SELECT * FROM countries");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+$getCountry=trim(filter_var(htmlspecialchars($_GET['country']), FILTER_SANITIZE_STRING));
+$country= $conn->query("SELECT * FROM countries WHERE name LIKE '%$getCountry%'");
+$country_x= $country->fetchAll(PDO::FETCH_ASSOC);
+
+$getContext =trim(filter_var(htmlspecialchars($_GET['context']), FILTER_SANITIZE_STRING)); 
+$cities= $conn->query("SELECT cities.name, cities.district, cities.population FROM cities JOIN countries  ON countries.code = cities.country_code WHERE countries.name LIKE '%$getCountry%'");
+$cities_x= $cities->fetchAll(PDO::FETCH_ASSOC);
+
 ?>
-<ul>
+
+<?php if ($getcoun && !isset($_GET['context'])); ?>
+<table> 
+  <tr>
+    <th> Country Name</th>
+    <th> Continent </th>
+    <th> Indenpendence Year</th>  
+    <th> Head of State</th>  
+  </tr>
+
+  <tbody>
 <?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+  <tr>
+          <td><?php echo $row['name'];?></td>
+          <td><?php echo $row['continent'];?></td>
+          <td><?php echo $row['independence_year'];?></td>
+          <td><?php echo $row['head_of_state'];?></td>
+        </tr>
+      <?php endforeach; ?>
+    </tbody>
+</table>
+
+<?php elseif ($getcoun && isset($_GET['context'])):?>
+  <table>
+    <tr> 
+      <th> Name</th>
+      <th> District</th>
+      <th> population </th>
+ </tr>
+<tbody>
+  <?php foreach ($results as $row): ?>
+    <tr>
+      <td> <?php echo $row ['name'];?></td>
+      <td><?php echo $row ['district']; ?></td>
+      <td><?php echo $row['population'];?></td>
+    </tr>
+    <?php endforeach; ?>
+            </tbody>
+        </table>
+        <?php endif; ?>
